b5ebae3c3009ca2513de217b1a1fdc3b39a18abf
Nadezhda Ugay <777ugay@gmail.com>
2023-06-21 16:34:19 +0300

    Homework2: add functions & tests

--- src/item.py
+++ src/item.py
@@ -1,3 +1,8 @@
+import csv
+
+FILE_PATH = 'D:\\Study\\DZ\\electronics-shop-project\\src\\items.csv'
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,11 +18,24 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
 
         Item.all.append(self)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and len(name) <= 10:
+            self.__name = name
+        elif isinstance(name, str) and len(name) > 10:
+            self.__name = name[:10]
+        else:
+            raise ValueError('Некорректное название')
 
     def calculate_total_price(self) -> float:
         """
@@ -32,3 +50,25 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls):
+        Item.all.clear()
+        with open(FILE_PATH, newline='') as f:
+            reader = csv.DictReader(f)
+            for row in reader:
+                name = row["name"]
+                price = float(row['price'])
+                quantity = int(row['quantity'])
+                cls(name, price, quantity)
+
+    @staticmethod
+    def string_to_number(string):
+        if string.isdigit():
+            return int(string)
+        else:
+            try:
+                float(string)
+                return int(float(string))
+            except ValueError:
+                raise ValueError
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,23 +1,56 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+import pytest
+
 from src.item import Item
 
-item1 = Item("Смартфон", 10000, 20)
-item2 = Item("Ноутбук", 20000, 5)
-item3 = Item("Кабель", 10, 5)
+
+@pytest.fixture
+def item1():
+    item1 = Item("Смартфон", 10000, 20)
+    return item1
 
 
-def test_item():
+@pytest.fixture
+def item2():
+    item2 = Item("Ноутбук", 20000, 5)
+    return item2
+
+
+@pytest.fixture
+def item3():
+    item3 = Item("Кабель", 10, 5)
+    return item3
+
+
+@pytest.fixture
+def item():
+    item = Item('Телефон', 10000, 5)
+    return item
+
+
+def test_item(item1):
     assert item1.name == 'Смартфон'
     assert item1.price == 10000
     assert item1.quantity == 20
 
 
-def test_calculate_total_price():
+def test_property_setter_name(item):
+    item.name = 'Смартфон'
+    assert item.name == 'Смартфон'
+    item.name = "СуперСмартфон"
+    assert item.name == "СуперСмарт"
+    item.name = '123'
+    assert item.name == '123'
+    with pytest.raises(ValueError):
+        item.name = 123
+
+
+def test_calculate_total_price(item1, item2):
     assert item1.calculate_total_price() == 200000
     assert item2.calculate_total_price() == 100000
 
 
-def test_apply_discount():
+def test_apply_discount(item1, item2, item3):
     Item.pay_rate = 0.8
     item3.pay_rate = 0.5
     item1.apply_discount()
@@ -27,5 +60,18 @@ def test_apply_discount():
     assert item3.price == 5
 
 
-def test_list_items():
-    assert Item.all == [item1, item2, item3]
+def test_instantiate_from_csv(item3):
+    Item.instantiate_from_csv()
+    assert Item.all[3].name == 'Мышка'
+    assert Item.all[3].price == 50
+    assert Item.all[3].quantity == 5
+    assert len(Item.all) == 5
+    assert Item.all[0].name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('123') == 123
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
+    with pytest.raises(ValueError):
+        Item.string_to_number('abc123')
